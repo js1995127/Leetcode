9482153e6d8b7c8e775c83717e69931c6a790278
DESKTOP-UUT0PAB\Shan <564563594@qq.com>
2017-08-02 14:33:36 +0800

    Add test template

--- Test/test.c
+++ Test/test.c
@@ -1,8 +1,43 @@
 #include <stdio.h>
+#include "solution.h"
 
-using namespace std;
+void case_begin(int case_num)
+{   
+    printf("    Case %d begin\n", case_num);
+}
 
-int main(){
-    printf("hello world!");
+void case_end(int case_num)
+{   
+    printf("    Case %d end\n", case_num);
+}
+
+void print_output()
+{
+    printf("        Output: ");
+}
+
+void print_expect()
+{
+    printf("        Expect: ");
+}
+
+int main()
+{
+    printf("Test for {function}\n");
+    printf("Test begin\n");
+
+    int case_num;
+
+    case_num = {case_num};
+    case_begin(case_num);
+    {
+        print_output();
+        printf("%{format}\n", {function});
+        print_expect();
+        printf("{expect result}\n");
+    }
+    case_end(case_num);
+    
+    printf("Test end\n");
     return 0;
 }
